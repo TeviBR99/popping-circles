554a1d495d439eb1f61b4b882067eb4f35587f2d
TeviBR99 <92033034+TeviBR99@users.noreply.github.com>
2025-01-03 23:39:20 +0100

    Created random bubbles

--- game-scripts/game.js
+++ game-scripts/game.js
@@ -2,7 +2,6 @@ let ballsToRemove = 0
 let clockIsTicking = true
 const TIME_INTERVAL_IN_SECONDS = 1
 let timerInterval
-const addZero = (time) => { return time < 10 ? '0' : '' }
 
 window.onload = function(){
     document.querySelector('.stopwatch').innerHTML = '00:00'
@@ -33,7 +32,7 @@ window.onload = function(){
         }
         document.getElementById("game-options").style.display = 'none'
         startStopWatch()
-        drawBalls()
+        drawBubbles()
     })
     
 }
@@ -41,16 +40,32 @@ window.onload = function(){
 function startStopWatch(){
     let seconds = 0
     let minutes = 0
+    const addZero = (time) => { return time < 10 ? '0' : '' }
     timerInterval = setInterval(() => {
         if(clockIsTicking){
             minutes = seconds === 59 ? minutes + 1 : minutes + 0
             seconds = seconds === 59 ? 0 : seconds + 1
-            
             document.querySelector('.stopwatch').innerHTML = `${addZero(minutes)}${minutes}:${addZero(seconds)}${seconds}`
         }
     }, TIME_INTERVAL_IN_SECONDS * 1000);
 }
 
-function drawBalls(){
+function drawBubbles(){
+    for(let i=0; i<ballsToRemove; i++){
+        const xAxysPx = getRandomNumber(800)
+        const yAxysPx = getRandomNumber(800)
+        const gameBoard = document.getElementById('game-board')
+        const bubbleDiv = document.createElement('div')
 
+        bubbleDiv.classList.add('red-bubble')
+        bubbleDiv.style.width = 20
+        bubbleDiv.style.height = 30
+        bubbleDiv.style.transform = `translate(${xAxysPx}px, ${yAxysPx}px)`
+    
+        gameBoard.appendChild(bubbleDiv)
+    }
+}
+
+function getRandomNumber(maxWidth){
+    return Math.floor(Math.random() * maxWidth) + 1;
 }
