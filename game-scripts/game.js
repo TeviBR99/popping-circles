0a9702cd8ea0c2b53374dbd1980176ad05994e8d
TeviBR99 <92033034+TeviBR99@users.noreply.github.com>
2025-01-03 22:21:25 +0100

    Modifying buttons behaviour

--- game-scripts/game.js
+++ game-scripts/game.js
@@ -3,12 +3,31 @@ let ballsToRemove = 0
 
 window.onload = function(){
     const ballsSelector = document.getElementById("balls-selector")
+    const buttonRemoveAllColors = document.getElementById('button-remove-all-colors')
+    const buttonRemoveOneColor = document.getElementById('button-remove-one-color')
+
     ballsSelector.addEventListener("change", () =>{
-        ballsToRemove = Number(selector.value)
+        ballsToRemove = Number(ballsSelector.value)
+        console.log(ballsToRemove)
     }) 
 
+    buttonRemoveOneColor.addEventListener("click", () =>{
+        buttonRemoveAllColors.disabled = true
+    })
+
+    buttonRemoveAllColors.addEventListener("click", () =>{
+        buttonRemoveOneColor.disabled = true
+    })
+
     document.querySelector('.play-button').addEventListener("click", () =>{
-        console.log(play)
+        console.log('play')
+        if(buttonRemoveAllColors.disabled){
+            buttonRemoveAllColors.disabled = false
+        }
+
+        if(buttonRemoveOneColor.disabled){
+            buttonRemoveOneColor.disabled = false
+        }
     })
     
 }
