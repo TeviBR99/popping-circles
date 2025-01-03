94664bb1e0cb73260731dc23e29d42db26fea881
TeviBR99 <92033034+TeviBR99@users.noreply.github.com>
2025-01-03 23:09:19 +0100

    Started stopwatch

--- game-scripts/game.js
+++ game-scripts/game.js
@@ -1,7 +1,11 @@
 let ballsToRemove = 0
-
+let clockIsTicking = true
+const TIME_INTERVAL_IN_SECONDS = 1
+let timerInterval
+const addZero = (time) => { return time < 10 ? '0' : '' }
 
 window.onload = function(){
+    document.querySelector('.stopwatch').innerHTML = '00:00'
     const ballsSelector = document.getElementById("balls-selector")
     const buttonRemoveAllColors = document.getElementById('button-remove-all-colors')
     const buttonRemoveOneColor = document.getElementById('button-remove-one-color')
@@ -20,7 +24,6 @@ window.onload = function(){
     })
 
     document.getElementById('play-button').addEventListener("click", () =>{
-        console.log('play')
         if(buttonRemoveAllColors.disabled){
             buttonRemoveAllColors.disabled = false
         }
@@ -29,12 +32,25 @@ window.onload = function(){
             buttonRemoveOneColor.disabled = false
         }
         document.getElementById("game-options").style.display = 'none'
-
+        startStopWatch()
         drawBalls()
     })
     
 }
 
+function startStopWatch(){
+    let seconds = 0
+    let minutes = 0
+    timerInterval = setInterval(() => {
+        if(clockIsTicking){
+            minutes = seconds === 59 ? minutes + 1 : minutes + 0
+            seconds = seconds === 59 ? 0 : seconds + 1
+            
+            document.querySelector('.stopwatch').innerHTML = `${addZero(minutes)}${minutes}:${addZero(seconds)}${seconds}`
+        }
+    }, TIME_INTERVAL_IN_SECONDS * 1000);
+}
+
 function drawBalls(){
-    
+
 }
